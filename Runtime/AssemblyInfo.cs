9827ec2c12337478c5778dfa88eb5fb1ca989f24
V0odo0 <Voodoo2211@gmail.com>
2024-09-27 05:51:44 +0500

    1.0.1

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -1,3 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("Knot.Core.Editor")]
 [assembly: InternalsVisibleTo("Knot.Localization")]
+[assembly: InternalsVisibleTo("Knot.Localization.Editor")]
